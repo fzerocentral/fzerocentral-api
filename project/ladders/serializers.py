aad632378af6ed6c3d46420c563c79a598781692
JoselleAstrid <JoselleAstrid@users.noreply.github.com>
2022-04-29 20:47:01 -0700

    When fetching Ladders, allow the associated Game to be included.

--- project/ladders/serializers.py
+++ project/ladders/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework_json_api import serializers
 
+from games.serializers import GameSerializer
 from .models import Ladder
 
 
 class LadderSerializer(serializers.ModelSerializer):
+    # These related fields are available for inclusion with the `include`
+    # query parameter.
+    included_serializers = {
+        'game': GameSerializer,
+    }
+
     class Meta:
         model = Ladder
         exclude = ['date_created', 'date_modified']
