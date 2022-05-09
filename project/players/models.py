bb6d856a1583c5979d663ef96fc8d5c5e6273cec
JoselleAstrid <JoselleAstrid@users.noreply.github.com>
2022-05-08 18:26:24 -0700

    Increase username limit from 20 to 25 chars, to accommodate existing FZC usernames.

--- project/players/models.py
+++ project/players/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Player(models.Model):
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=25)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
